7d98bcd524b1922856d118dd8caa0237c3cf88ff
Rouven Bissinger <info@rouven.xyz>
2022-10-23 10:00:05 +0200

    implemet and test s-box1

--- des.py
+++ des.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 
+from sqlalchemy import column
+
 
 def genPseudoRandomKey():
     "Returns a pseudo random 64 bit np.array"
@@ -58,3 +60,12 @@ class DES:
     
     def xor_48(x,k):
         return np.bitwise_xor(x.reshape((48,)),k.reshape((48,))).reshape((6,8))
+    
+    def s1(x):
+        a = np.array([[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7], \
+                    [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8], \
+                    [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0], \
+                    [15, 12, 8, 2,4 ,9 ,1 ,7 , 5, 11, 3, 14, 10, 0, 6, 13]], dtype=np.uint8)
+        row = np.packbits(np.array((0,0,0,0,0,0,x[0], x[-1]), dtype=np.uint8))[0]
+        column = np.packbits(np.append(np.zeros((4,), dtype=np.uint8), x[1:5]))[0]
+        return np.unpackbits(a[row][column])[-4:]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import des
 import numpy as np
 if __name__ == "__main__":
-   
+   print(des.DES.s1(np.array((1,0,0,1,0,1))))
